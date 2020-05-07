a7e203ff06811b124c5b7c3b7ff8bbf1253ac6e5
6AHK0MAT <mail.ru>
2020-05-07 13:11:18 +0700

    RU-EN READY PROJECT

--- src/index.js
+++ src/index.js
@@ -8,17 +8,12 @@ const LanguageConsumer = LanguageContext.Consumer;
 class LanguageProvider extends React.Component {
     state = {
         language: "russian",
-        dictionary: {
-            russian: "Привет, Виталя!",
-            french: "Bonjour, Vitaliy!",
-            english: "Hello, Vitaliy!",
-            italian: "Ciao, Vitaliy!"
-        },
     };
 
     updateLanguage = e => this.setState({language: e.target.value});
 
     render() {
+        console.log(this.props);
         return (
             <LanguageContext.Provider
                 value={{
@@ -38,7 +33,6 @@ const Header = () => {
             {({updateLanguage}) => (
                 <header>
                     {/*see this site in*/}
-                    {/*{({language}) => props.dictionaryText[language]}*/}
                     <select onChange={updateLanguage}>
                         <option value="russian">русский</option>
                         <option value="french">french</option>
@@ -57,6 +51,40 @@ const TranslatableText = props => (
     </LanguageConsumer>
 );
 
+const TranslatableText2 = props => (
+
+    <LanguageConsumer>
+
+        {({language}) => {
+            return (
+                <table border="1" width="100%" cellPadding="5">
+                    <tr>
+                        <th>{props.dictionaryText2[language][0].head1}</th>
+                        <th>{props.dictionaryText2[language][0].head2}</th>
+                    </tr>
+                    <tr>
+                        <td>{props.dictionaryText2[language][1].text1}</td>
+                        <td>{props.dictionaryText2[language][1].text2}</td>
+                    </tr>
+                    <tr>
+                        <td>{props.dictionaryText2[language][2].text1}</td>
+                        <td>{props.dictionaryText2[language][2].text2}</td>
+                    </tr>
+                    <tr>
+                        <td>{props.dictionaryText2[language][3].text1}</td>
+                        <td>{props.dictionaryText2[language][3].text2}</td>
+                    </tr>
+                </table>
+
+
+
+            )
+        }
+        }
+
+    </LanguageConsumer>
+);
+
 const App = () => (
     <LanguageProvider>
         <div>
@@ -64,16 +92,42 @@ const App = () => (
             <h1>
                 <TranslatableText
                     dictionaryText={{
-                        russian: "Привет, Виталя!",
-                        french: "Bonjour, Vitaliy!",
-                        english: "Hello, Vitaliy!",
-                        italian: "Ciao, Vitaliy!"
+                        russian: 'Пример перевода таблицы',
+                        french: "Exemple de traduction de table",
+                        english: "Table translation example!",
+                        italian: "Esempio di traduzione della tabella",
                     }}
-                    dictionaryHead={{
-                        russian: "Посмотреть сайт на",
-                        french: "Bonjour, Vitaliy!",
-                        english: "Hello, Vitaliy!",
-                        italian: "Ciao, Vitaliy!"
+                />
+                <TranslatableText2
+                    dictionaryText2={{
+                        russian: [
+                            {head1: 'Заголовок 1', head2: 'Заголовок 2'},
+                            {text1: 'Строка 1', text2: 'Текст 1'},
+                            {text1: 'Строка 2', text2: 'Текст 2'},
+                            {text1: 'Строка 3', text2: 'Текст 3'},
+
+                        ],
+                        french: [
+                            {head1: 'Gros titre 1', head2: 'Gros titre 2'},
+                            {text1: 'Ligne 1', text2: 'Texte 1'},
+                            {text1: 'Ligne 2', text2: 'Texte 2'},
+                            {text1: 'Ligne 3', text2: 'Texte 3'},
+
+                        ],
+                        english: [
+                            {head1: 'Head 1', head2: 'Head 2'},
+                            {text1: 'Line 1', text2: 'Text 1'},
+                            {text1: 'Line 2', text2: 'Text 2'},
+                            {text1: 'Line 3', text2: 'Text 3'},
+
+                        ],
+                        italian: [
+                            {head1: 'Titolo 1', head2: 'Titolo 2'},
+                            {text1: 'Linea 1', text2: 'Testo 1'},
+                            {text1: 'Linea 2', text2: 'Testo 2'},
+                            {text1: 'Linea 3', text2: 'Testo 3'},
+
+                        ],
                     }}
                 />
             </h1>
